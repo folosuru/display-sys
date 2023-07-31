e88a9d76b1ca9e32ee85ef53ea6da8295328b074
folosuru <77926718+folosuru@users.noreply.github.com>
2023-07-31 11:15:28 +0900

    add program of train-view

---
+++ train-view/main.ts
@@ -0,0 +1,81 @@
+const Data_set: ({ room_name: string; project_name: string }[]) = [
+    {
+        "room_name": "3-1",
+        "project_name" : "ああああああああああ"
+    },
+    {
+        "room_name": "3-2",
+        "project_name" : "いいいいいいいいいい"
+    },
+    {
+        "room_name": "3-3",
+        "project_name" : "うううううううううう"
+    },
+    {
+        "room_name": "3-4",
+        "project_name" : "ええええええええええ"
+    },
+    {
+        "room_name": "3-5",
+        "project_name" : "おおおおおおおおおお"
+    }
+]
+
+
+function genInfoText(str: string): HTMLSpanElement {
+    const result = document.createElement("span");
+    result.classList.add("info_text");
+    result.innerText = str;
+    return result;
+}
+
+
+function addAppear(obj : HTMLSpanElement){
+    obj.classList.add("appear");
+    return obj;
+}
+
+
+function changeDisplay(index :number){
+    const before = document.getElementsByClassName("info_text");
+    Array.prototype.forEach.call(before, function(item) {
+        item.classList.add("disappear");
+    });
+    setTimeout(removeOld,740)
+    if (index >= Data_set.length){
+        index = 0;
+    }
+    document.getElementsByClassName("room_name")[0].appendChild(
+        addAppear(genInfoText(Data_set[index]["room_name"]))
+    )
+    document.getElementsByClassName("project_name")[0].appendChild(
+        addAppear(genInfoText(Data_set[index]["project_name"]))
+    )
+    setTimeout(removeOld,495);
+    setTimeout(changeSelectedRoom,500,Data_set[index]["room_name"]);
+
+    setTimeout(changeDisplay,3000,index+1)
+
+
+    return
+}
+
+function changeSelectedRoom(room_name : string) {
+    document.getElementsByClassName("map_selected")[0].classList.remove("map_selected");
+    document.getElementsByClassName(room_name)[0].classList.add("map_selected");
+}
+
+function removeOld() : void{
+    const old_list = document.getElementsByClassName("disappear");
+    while (old_list.length) {
+        console.log(old_list.item(0))
+        old_list.item(0).remove()
+    }
+}
+
+
+function init() : void{
+    setTimeout(changeDisplay,1000,1)
+}
+
+init()
