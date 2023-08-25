fd3baec9209430246d8ccd1b22110dbf99e5a246
folosuru <folosuru@gmail.com>
2023-08-25 14:17:27 +0900

    おかしい時に再取得

--- story-view-client/script/ScriptUpdater.ts
+++ story-view-client/script/ScriptUpdater.ts
@@ -11,6 +11,8 @@ export class ScriptUpdater {
     private ScriptManager :StoryScriptManager;
     private ScriptBuilder :ScriptBuilder;
 
+    private this_script_hash :string = "";
+
     constructor(mng :StoryScriptManager, file :FileManager) {
         this.ScriptManager = mng;
         this.ScriptBuilder = new ScriptBuilder(file,mng);
@@ -21,16 +23,31 @@ export class ScriptUpdater {
     }
 
     add(json :ScriptsJsonData){
+        // Date.now()はミリ秒単位 ＝ Date.now() はUNIX時間の1000倍
         if (typeof json === "object" && json !== null) {
-            let scriptJsonData : ScriptJsonData[] = json.scripts;
-            let scripts :Array<StoryScript> = new Array<StoryScript>();
-            for (let item of scriptJsonData){
+            if (json["next_update"] * 1000 > Date.now()) {
+                console.log("ended scripts. retry...");
+                setTimeout(() => {
+                        this.getScripts();
+                    }, (Math.random() * 5000)  // 長くても5秒くらいで行きましょう
+                );
+            }
+            if (json["hash"] == this.this_script_hash) {
+                console.log("same hash. retry...");
+                setTimeout(() => {
+                        this.getScripts();
+                    }, (Math.random() * 5000)  // 長くても5秒くらいで行きましょう
+                );
+            }
+            let scriptJsonData: ScriptJsonData[] = json.scripts;
+            let scripts: Array<StoryScript> = new Array<StoryScript>();
+            for (let item of scriptJsonData) {
                 scripts.push(this.ScriptBuilder.build(item));
             }
             this.ScriptManager.add(scripts);
+            setTimeout(() => {
+                this.getScripts();
+            }, (json.next_update * 1000) - Date.now())
         }
-        setTimeout(()=>{
-            this.getScripts();
-        },(json.next_update * 1000 ) - Date.now())
     }
 }
